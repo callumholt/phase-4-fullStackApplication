62cef05c224b219a91958010acb8dc7cc4fc106c
callum <callumpholt@gmail.com>
2023-06-19 20:50:19 +1000

    connected react component to be able to take email input

--- client/src/login_form.js
+++ client/src/login_form.js
@@ -2,46 +2,47 @@ import React, { useState } from "react";
 
 function LoginForm() {
   const [email, setEmail] = useState("");
-  //   const [category, setCategory] = useState("");
-  //   const [user, setUser] = useState("");
 
   const handleSubmit = async (event) => {
     event.preventDefault();
 
-    //     const response = await fetch("http://localhost:4567/todo", {
-    //       method: "POST",
-    //       headers: { "Content-Type": "application/json" },
-    //       body: JSON.stringify({
-    //         todo: todo,
-    //         category: category,
-    //         user: user,
-    //       }),
-    //     });
-    //     if (response.ok) {
-    //       console.log("Data successfully sent to database!");
-    //       alert("Data successfully sent to database!");
-    //     } else {
-    //       console.error("Error sending data to database:", response.status);
-    //     }
-    //   };
+    // Uncomment the following code when you're ready to send the data to the server
+    /*
+    const response = await fetch("http://localhost:4567/todo", {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify({
+        todo: todo,
+        category: category,
+        user: user,
+      }),
+    });
 
-    return (
-      <div>
-        <h2>
-          Please enter in your Todo that you would like to get done and your
-          name as ID..
-        </h2>
+    if (response.ok) {
+      console.log("Data successfully sent to the database!");
+      alert("Data successfully sent to the database!");
+    } else {
+      console.error("Error sending data to the database:", response.status);
+    }
+    */
+  };
 
-        <form onSubmit={handleSubmit}>
-          <label>
-            Email:
-            <input
-              type="text"
-              value={email}
-              onChange={(e) => setEmail(e.target.value)}
-            />
-          </label>
-          {/* <br />
+  return (
+    <div>
+      <h2>Please enter your email to login:</h2>
+
+      <form onSubmit={handleSubmit}>
+        <label>
+          Email:
+          <input
+            type="text"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+          />
+        </label>
+        {/* 
+        Uncomment the following code when you're ready to add the category and user inputs
+        <br />
         <label>
           Category:
           <input
@@ -59,12 +60,12 @@ function LoginForm() {
             onChange={(e) => setUser(e.target.value)}
           />
         </label>
-        <br /> */}
-          <button type="submit">Submit</button>
-        </form>
-      </div>
-    );
-  };
+        <br />
+        */}
+        <button type="submit">Submit</button>
+      </form>
+    </div>
+  );
 }
 
 export default LoginForm;
