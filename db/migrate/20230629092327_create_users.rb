417482e270ccb72bc7c9843228752c929ef84955
callum <callumpholt@gmail.com>
2023-07-13 06:54:33 +1000

    updates

--- db/migrate/20230629092327_create_users.rb
+++ db/migrate/20230629092327_create_users.rb
@@ -2,7 +2,6 @@ class CreateUsers < ActiveRecord::Migration[6.1]
   def change
     create_table :users do |t|
       
-      t.text :username
       t.text :name
       t.text :bio
       t.integer :postcode
